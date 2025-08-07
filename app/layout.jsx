eafed692b25bd3f010fcb32545a37fea0e79c788
elisha <elishajameel2007@gmail.com>
2025-08-07 16:11:36 +0500

    apply new font in the app

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,10 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const Jetbrains_mono = JetBrains_Mono({
+  variable: "--font-jetbrainsMono",
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
 });
 
 export const metadata = {
@@ -20,7 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${Jetbrains_mono.variable} antialiased`}
       >
         {children}
       </body>
