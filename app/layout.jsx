ae140785dace31552507ee1b9afc8fa91e053c64
elisha <elishajameel2007@gmail.com>
2025-08-07 16:19:22 +0500

    created header and use it in the layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
 
 const Jetbrains_mono = JetBrains_Mono({
   variable: "--font-jetbrainsMono",
@@ -18,6 +19,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${Jetbrains_mono.variable} antialiased`}
       >
+        <Header/>
         {children}
       </body>
     </html>
