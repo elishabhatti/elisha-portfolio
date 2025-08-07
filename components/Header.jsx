c6c02c321339eecadd39457c1a8f915636fa9dcb
elisha <elishajameel2007@gmail.com>
2025-08-07 16:26:20 +0500

    added nav

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,24 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+import Nav from "./Nav";
+
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="py-4 xl:py-8 text-white">
+      <div className="container mx-auto">
+        {/* Logo */}
+        <Link href="/">
+          <h1 className="text-3xl font-semibold">
+            Elisha<span className="text-accent">.</span>
+          </h1>
+        </Link>
+        {/* desktop nav */}
+        <div>
+        <Nav/>
+        </div>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
