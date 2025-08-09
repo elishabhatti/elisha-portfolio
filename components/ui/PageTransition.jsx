cfe2879cea2ba1f98735cc94b05ab480d24ce260
elisha <elishajameel2007@gmail.com>
2025-08-09 12:37:14 +0500

    installed framer motion

--- components/ui/PageTransition.jsx
+++ components/ui/PageTransition.jsx
@@ -1,11 +1,24 @@
+"use client";
+import { AnimatePresence, motion } from "framer-motion";
+import { usePathname } from "next/navigation";
 
-
-const PageTransition = ({children}) => {
+const PageTransition = ({ children }) => {
+  const pathname = usePathname();
   return (
     <AnimatePresence>
-    <div>PageTransition</div>
+      <div key={pathname}>
+        <motion.div
+          className="h-screen w-full fixed bg-primary top-0 pointer-events-none"
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 0,
+            transition: { delay: 1, duration: 0.4, ease: "easeInOut" },
+          }}
+        ></motion.div>
+        {children}
+      </div>
     </AnimatePresence>
-  )
-}
+  );
+};
 
-export default PageTransition
+export default PageTransition;
