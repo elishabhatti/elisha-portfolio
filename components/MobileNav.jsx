c9c6326bcaddfc01ef924eb69d5c8668e078252b
elisha <elishajameel2007@gmail.com>
2025-08-09 13:42:54 +0500

    created mobile nav

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -2,7 +2,7 @@
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
-import { CiMenuFries } from "react-icons/ci"
+import { CiMenuFries } from "react-icons/ci";
 
 const links = [
   {
@@ -28,7 +28,39 @@ const links = [
 ];
 
 const MobileNav = () => {
-  return <nav></nav>;
+  const pathname = usePathname();
+  return (
+    <Sheet>
+      <SheetTrigger className="flex justify-center items-center">
+        <CiMenuFries className="text-[32px] text-accent" />
+      </SheetTrigger>
+      <SheetContent className="flex flex-col p-5">
+        <div className="mt-32 text-center text-2xl">
+          <Link href="/">
+            <h1>
+              Elisha<span className="text-cyan-600 text-2xl">.</span>
+            </h1>
+          </Link>
+        </div>
+        <nav className="flex flex-col justify-center items-center gap-4">
+          {links.map((link, index) => {
+            return (
+              <Link
+                href={link.path}
+                className={` ${
+                  link.path === pathname &&
+                  "text-cyan-600 border-b-2 border-cyan-600"
+                } text-md capitalize hover:text-cyan-900 transition-all`}
+                key={index}
+              >
+                {link.name}
+              </Link>
+            );
+          })}
+        </nav>
+      </SheetContent>
+    </Sheet>
+  );
 };
 
 export default MobileNav;
