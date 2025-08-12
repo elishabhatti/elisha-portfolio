74e4afab5a19c2d422597aa5e87fc69233ea61c4
elisha <elishajameel2007@gmail.com>
2025-08-12 23:37:36 +0500

    added stats content

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -1,9 +1,35 @@
-"use client"
+"use client";
+
+import CountUp from "react-countup";
+
+const stats = [
+  { num: 12, text: "Years of Experience" },
+  { num: 26, text: "Project Completed" },
+  { num: 8, text: "Technologies mastered" },
+  { num: 500, text: "Code commits" },
+];
 
 const Stats = () => {
   return (
-    <div>Stats</div>
-  )
-}
+    <section>
+      <div className="container mx-auto">
+        <div>
+          {stats.map((item, index) => {
+            return (
+              <div key={index}>
+                <CountUp
+                  end={item.num}
+                  duration={5}
+                  delay={2}
+                  className="text-4xl xl:text-3xl font-extrabold"
+                />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Stats
+export default Stats;
