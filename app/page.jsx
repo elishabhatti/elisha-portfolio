0d20ae07175c689649154131a9fc504116624e0d
elisha <elishajameel2007@gmail.com>
2025-08-12 12:25:17 +0500

    added the text content

--- app/page.jsx
+++ app/page.jsx
@@ -3,15 +3,20 @@ import { FiDownLoad } from "react-icons/fi";
 
 const page = () => {
   return (
-    <section className="w-full">
+    <section className="w-full container">
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 pb-24">
           {/* text */}
           <div className="text-center xl:text-left">
-            <span>Web Developer</span>
+            <span className="text-xl">Web Developer</span>
             <h1 className="h1">
-              Hello I,m <br /> <span>Elisha Jameel</span>
+              Hello I,m <br />{" "}
+              <span className="text-green-300">Elisha Jameel</span>
             </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">
+              I excel crafting elegant digital experience and i am proficient in
+              various programming languages and technologies
+            </p>
           </div>
           {/* photo */}
           <div>photo</div>
