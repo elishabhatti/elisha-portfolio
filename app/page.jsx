c001368ec800ce38a43ee0d1086a188e56856067
elisha <elishajameel2007@gmail.com>
2025-08-12 12:17:24 +0500

    added heading of the text

--- app/page.jsx
+++ app/page.jsx
@@ -6,7 +6,14 @@ const page = () => {
     <section className="w-full">
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 pb-24">
-          <div>text</div>
+          {/* text */}
+          <div className="text-center xl:text-left">
+            <span>Web Developer</span>
+            <h1 className="h1">
+              Hello I,m <br /> <span>Elisha Jameel</span>
+            </h1>
+          </div>
+          {/* photo */}
           <div>photo</div>
         </div>
       </div>
