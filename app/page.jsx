e2791b24414d1606eefabfd34bba6f2edd5867cb
elisha <elishajameel2007@gmail.com>
2025-08-12 12:15:31 +0500

    created the base of the home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,17 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { FiDownLoad } from "react-icons/fi";
 
-export default function Home() {
+const page = () => {
   return (
-    <main>homepage</main>
+    <section className="w-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 pb-24">
+          <div>text</div>
+          <div>photo</div>
+        </div>
+      </div>
+    </section>
   );
-}
+};
+
+export default page;
