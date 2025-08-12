0b37e05c4a17721318513724a7f9e64cdb7dcfe0
elisha <elishajameel2007@gmail.com>
2025-08-12 23:43:09 +0500

    added stats content and styling

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -3,26 +3,36 @@
 import CountUp from "react-countup";
 
 const stats = [
-  { num: 12, text: "Years of Experience" },
-  { num: 26, text: "Project Completed" },
+  { num: 3, text: "Years of Experience" },
+  { num: 71, text: "Project Completed" },
   { num: 8, text: "Technologies mastered" },
-  { num: 500, text: "Code commits" },
+  { num: 1083, text: "Code commits" },
 ];
 
 const Stats = () => {
   return (
     <section>
       <div className="container mx-auto">
-        <div>
+        <div className="flex flex-wrap gap-3 max-w-[80vw] mx-auto xl:max-w-none">
           {stats.map((item, index) => {
             return (
-              <div key={index}>
+              <div
+                className="flex-1 flex gap-4 items-center justify-center xl:justify-start"
+                key={index}
+              >
                 <CountUp
                   end={item.num}
                   duration={5}
                   delay={2}
                   className="text-4xl xl:text-3xl font-extrabold"
                 />
+                <p
+                  className={`${
+                    item.text.length < 15 ? "max-w-[100px]" : "max-w-[150px]"
+                  } leading-snug text-white/80`}
+                >
+                  {item.text}
+                </p>
               </div>
             );
           })}
