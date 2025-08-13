39cb914397c78b6aa9f6f40f24718d091badae32
elisha <elishajameel2007@gmail.com>
2025-08-13 12:14:55 +0500

    created services content

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,3 +1,11 @@
+import { BsArrowDownRight } from "react-icons/bs"
+
+const services = [
+  {num: '01', title: "Web Development", description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque tenetur", href:""},
+  {num: '02', title: "UI/UX Design", description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque tenetur", href:""},
+  {num: '03', title: "Logo Design", description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque tenetur", href:""},
+  {num: '04', title: "SEO", description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque tenetur", href:""},
+]
 const Services = () => {
   return <div>Services Page</div>;
 };
