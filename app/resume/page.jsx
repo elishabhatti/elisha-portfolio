1c32ab2eccfd9f564c3f2ab9948102a24dcb080a
elisha <elishajameel2007@gmail.com>
2025-08-13 13:06:14 +0500

    added about content

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,7 +1,32 @@
-const Resume = () => {
-  return (
-    <div>Resume</div>
-  )
-}
+"use client";
+import {
+  FaHtml5,
+  FaCss3,
+  FaJs,
+  FaReact,
+  FaFigma,
+  FaNodeJs,
+} from "react-icons/fa";
+import { SiTailwindcss, SiNextdotjs } from "react-icons/si";
 
-export default Resume
+// about data
+const about = {
+  title: "About me",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga esse autem, delectus incidunt debitis doloribus quam libero.",
+  info: [
+    { fieldName: "Name", fieldValue: "Elisha Jameel" },
+    { fieldName: "Phone", fieldValue: "(+92) 309 2772847" },
+    { fieldName: "Experience", fieldValue: "2+ Years" },
+    { fieldName: "Nationality", fieldValue: "Pakistani" },
+    { fieldName: "Email", fieldValue: "elishajameel270@gmail.com" },
+    { fieldName: "Freelance", fieldValue: "Available" },
+    { fieldName: "Languages", fieldValue: "English, Urdu" },
+  ],
+};
+
+const Resume = () => {
+  return <div></div>;
+};
+
+export default Resume;
