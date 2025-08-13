8aa11cb815f1c56769231fbd87eeec3d1b9e40d9
elisha <elishajameel2007@gmail.com>
2025-08-13 19:26:15 +0500

    when redirect to the link open in new tab

--- components/Social.jsx
+++ components/Social.jsx
@@ -10,7 +10,12 @@ const Social = ({ containerStyles, iconStyles }) => {
     <div className={containerStyles}>
       {socials.map((item, index) => {
         return (
-          <Link key={index} href={item.path} className={iconStyles}>
+          <Link
+            target="_blank"
+            key={index}
+            href={item.path}
+            className={iconStyles}
+          >
             {item.icon}
           </Link>
         );
