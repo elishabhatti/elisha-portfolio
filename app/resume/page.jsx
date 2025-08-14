b6e70dc409eaddb71542216d55747cba20f52437
elisha <elishajameel2007@gmail.com>
2025-08-14 13:47:49 +0500

    added group hover in sills icons

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -244,13 +244,13 @@ const Resume = () => {
                     <li key={index}>
                       <TooltipProvider delayDuration={100}>
                         <Tooltip>
-                          <TooltipTrigger className="w-full h-[150px] bg-[#232329] rounded-xl flex justify-center items-center">
+                          <TooltipTrigger className="w-full group h-[150px] bg-[#232329] rounded-xl flex justify-center items-center">
                             <div className="text-6xl group-hover:text-[#00ff99] transition-all duration-300">
                               {skill.icon}
                             </div>
                           </TooltipTrigger>
                           <TooltipContent>
-                            <p>{skill.name}</p>
+                            <p className="capitalize">{skill.name}</p>
                           </TooltipContent>
                         </Tooltip>
                       </TooltipProvider>
