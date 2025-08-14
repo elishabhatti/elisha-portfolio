d4d1143a725b1a88f1845987788655919a9db745
elisha <elishajameel2007@gmail.com>
2025-08-14 14:05:09 +0500

    added all the imports use in the work page

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,3 +1,19 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { BsArrowUpRight, BsGithub } from "react-icons/bs";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import Link from "next/link";
+import Image from "next/image";
+
 const Work = () => {
   return <div>Work</div>;
 };
