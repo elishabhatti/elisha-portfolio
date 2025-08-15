c6bf3c4ccbb1e841efae15f02df05a38efd78ab2
elisha <elishajameel2007@gmail.com>
2025-08-15 13:06:45 +0500

    added overlay in projects

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -137,7 +137,7 @@ const Work = () => {
                   <SwiperSlide className="w-full" key={index}>
                     <div className="h-[460px] relative group flex justify-center items-center bg-pink-50/20">
                       {/* overlay */}
-                      <div></div>
+                      <div className="absolute top-0 bottom-0 w-full h-full bg-black/10 z-10"></div>
                       {/* image */}
                       <div className="relative w-full h-full">
                         <Image
@@ -151,6 +151,7 @@ const Work = () => {
                   </SwiperSlide>
                 );
               })}
+              {/* slider buttons */}
             </Swiper>
           </div>
         </div>
