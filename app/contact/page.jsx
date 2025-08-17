d4178d605a5fa1a4167f3a4298118b6adf2c33d0
elisha <elishajameel2007@gmail.com>
2025-08-17 19:09:52 +0500

    added container and info styling

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -37,25 +37,28 @@ import { motion } from "framer-motion";
 
 const Contact = () => {
   return (
-    <motion.section 
-    initial={{ opacity: 0 }}
-    animate={{
-      opacity: 1,
-      transition: { delay: 1.4, duration: 0.4, ease: "easeIn" },
-    }}
-    className="py-6"
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 1.4, duration: 0.4, ease: "easeIn" },
+      }}
+      className="py-6"
     >
       <div className="container mx-auto">
-        <div>
+        <div className="flex flex-col xl:flex-row gap-[30px]">
           {/* form */}
-          <div>
+          <div className="xl:h-[54%] order-2 xl:order-none">
             <form>form</form>
           </div>
           {/* info */}
+          <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
+            info
+          </div>
         </div>
       </div>
     </motion.section>
-  ) 
+  );
 };
 
 export default Contact;
