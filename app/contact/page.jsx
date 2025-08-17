30461f9a123d48a60f6ed8b324eaf133ed390f27
elisha <elishajameel2007@gmail.com>
2025-08-17 20:51:57 +0500

    showed the social icons

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -45,10 +45,10 @@ const Contact = () => {
       }}
       className="py-6"
     >
-      <div className="container mx-auto">
+      <div className="container px-10 mx-auto">
         <div className="flex flex-col xl:flex-row gap-[30px]">
           {/* form */}
-          <div className="xl:h-[54%] order-2 xl:order-none">
+          <div className="xl:w-[54%] order-2 xl:order-none">
             <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
               <h3 className="text-4xl text-[#00ff99]">Let's work together</h3>
               <p className="text-white/60">
@@ -89,7 +89,24 @@ const Contact = () => {
           </div>
           {/* info */}
           <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
-            info
+            <ul>
+              {info.map((item, index) => {
+                return (
+                  <li
+                    key={index}
+                    className="flex items-center xl:justify-end order-1 gap-3 xl:order-none mb-8 xl:mb-0"
+                  >
+                    <div className="w-[52px] h-[52px] bg-[#27272c] text-[#00ff99] flex rounded-md items-center justify-center">
+                      <div className="text-[28px]">{item.icon}</div>
+                    </div>
+                    <div className="flex-1">
+                      <p className="text-white/60">{item.title}</p>
+                      <h3 className="text-xl">{item.description}</h3>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
           </div>
         </div>
       </div>
