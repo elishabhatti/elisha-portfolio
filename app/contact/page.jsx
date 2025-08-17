1e0954cf9a3ee16a4aa62db433f4edc236febcc4
elisha <elishajameel2007@gmail.com>
2025-08-17 18:58:16 +0500

    import all the stuff needed for contact page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,7 +1,24 @@
-const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+"use client";
 
-export default Contact
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa"
+
+
+
+const Contact = () => {
+  return <div>Contact</div>;
+};
+
+export default Contact;
