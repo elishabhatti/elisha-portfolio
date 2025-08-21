3b5fbdfebcea55dd6b02ab01293071ef4d18ce47
elisha <elishajameel2007@gmail.com>
2025-08-21 11:25:00 +0500

    change the color in header of the accent

--- components/Header.jsx
+++ components/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
         {/* Logo */}
         <Link href="/">
           <h1 className="text-3xl font-semibold">
-            Elisha<span className="text-[#00ff99]">.</span>
+            Elisha<span className="primary-text-color">.</span>
           </h1>
         </Link>
         {/* desktop nav */}
