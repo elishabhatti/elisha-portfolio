48f85b79edd0b2c695dbe4f21d4d790674c81977
elisha <elishajameel2007@gmail.com>
2025-08-21 11:12:58 +0500

    added cz-shortcut-listen="true" for removing the tree hydrated error

--- app/layout.jsx
+++ app/layout.jsx
@@ -18,7 +18,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${Jetbrains_mono.variable} antialiased`}>
+      <body cz-shortcut-listen="true" className={`${Jetbrains_mono.variable} antialiased`}>
         <Header />
         <StairTransition />
         <PageTransition>{children}</PageTransition>
