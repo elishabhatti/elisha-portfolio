738e1c4d912dd90b722de36b4a9304405faadbca
elisha <elishajameel2007@gmail.com>
2025-08-23 14:09:11 +0500

    showed less description

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -155,7 +155,7 @@ const Resume = () => {
               <div className="flex flex-col gap-[30px] text-center xl:text-left">
                 <h3 className="text-4xl font-bold">{experience.title}</h3>
                 <p className="max-w-[600px] text-white/60 max-auto xl:mx-0">
-                  {experience.description}
+                  {experience.description.length >= 100 ? experience.description.slice(0, 120) + "..." : experience.description }
                 </p>
                 <ScrollArea className="h-[350px]">
                   <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
