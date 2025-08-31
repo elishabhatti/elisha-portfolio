009c4776044f37d0a562b8518b0452f740a124f9
elisha <elishajameel2007@gmail.com>
2025-08-31 14:32:21 +0500

    removed h-screen home page section

--- components/pages/Home.jsx
+++ components/pages/Home.jsx
@@ -8,12 +8,13 @@ import { motion } from "framer-motion";
 
 const Home = () => {
   return (
-    <motion.section 
-          initial={{ opacity: 0, y: 50 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.6, ease: "easeOut" }}
-          viewport={{ once: true, amount: 0.2 }}
-    className="w-full h-screen">
+    <motion.section
+      initial={{ opacity: 0, y: 50 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.6, ease: "easeOut" }}
+      viewport={{ once: true, amount: 0.2 }}
+      className="w-full"
+    >
       <div className="container ">
         <div className="flex flex-col lg:flex-row items-center lg:items-center justify-center gap-6 pt-6 lg:gap-24 lg:pt-6 pb-14">
           {/* Text */}
